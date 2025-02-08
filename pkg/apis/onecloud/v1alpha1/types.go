1a7cb92ac03aadae46a4229edbba3d8e6269692f
Zexi Li <zexi.li@icloud.com>
2025-02-08 12:08:15 +0800

    feat(operator): add secure property of ObjectStoreConfig

--- pkg/apis/onecloud/v1alpha1/types.go
+++ pkg/apis/onecloud/v1alpha1/types.go
@@ -766,6 +766,7 @@ type ObjectStoreConfig struct {
 	Endpoint  string `json:"endpoint"`
 	AccessKey string `json:"accessKey"`
 	SecretKey string `json:"secretKey"`
+	Secure    bool   `json:"secure"`
 }
 
 type MonitorStackLokiSpec struct {
